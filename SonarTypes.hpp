eaab97d6fec556f3630ed18735742f056af2f71a
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2010-10-31 14:51:51 +0100

    labels

--- SonarTypes.hpp
+++ SonarTypes.hpp
@@ -1,3 +1,10 @@
+/**
+ * Author: Matthias Goldhoorn (matthias.goldhoorn@dfki.de)
+ * Company: Deutsches Forschungszentrum für Künstliche Intelligenz - Robotics Innovation Center (DFKI RIC)
+ * Year 2010
+ * Desc:
+ *
+*/
 #ifndef SONARTYPES_H
 #define SONARTYPES_H
 
